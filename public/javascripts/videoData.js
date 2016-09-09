be861abb3f4f49ca023bebeff0dd0865a1cff30f
annahuffman147 <annahuffman.plc@gmail.com>
2016-09-09 09:53:57 -0600

    fixed layout with positioning. Do NOT change again!

--- public/javascripts/videoData.js
+++ public/javascripts/videoData.js
@@ -1,5 +1,7 @@
 var videos = ['videos/babybayEvian.mp4', 'videos/babymeEvian.mp4', 'videos/rollerbabiesEvian.mp4']
 
+var videoURLs = ["https://www.youtube.com/embed/XQcVllWpwGs"]
+
 function playVideo() {
 
 }
