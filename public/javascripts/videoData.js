37bd1ec8b0c069eaf1693d2cf5e60b33353946f0
annahuffman147 <annahuffman.plc@gmail.com>
2016-09-09 21:25:17 -0600

    added activity log and change icon with clicks

--- public/javascripts/videoData.js
+++ public/javascripts/videoData.js
@@ -1,35 +1,120 @@
 "use strict";
-//
-
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/OWG3rtGoIlI" frameborder="0" allowfullscreen></iframe> surfingbabies 1:35
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/GpMchwEYHms" frameborder="0" allowfullscreen></iframe> babymeEvian 1:16
-// <iframe width="560" height="315" src="https://www.youtube.com/embed/XQcVllWpwGs" frameborder="0" allowfullscreen></iframe>rollerbabiesEvian 1:01
-
 
 var myVideo = document.getElementById("video");
 var source = document.getElementById('source');
+var activityLog = document.getElementById('log')
+var activityLogArray = []
 
-console.log(typeof myVideo.currentSrc);
+function playVideo() {
+  var time = myVideo.currentTime.toFixed(2)
+  var playLog = 'Time: ' + time + '   Video Played' + '<br>'
+  myVideo.play();
+  activityLogArray.unshift(playLog);
+  activityLog.innerHTML+= playLog;
+  document.getElementById("ballTurtleRabbit").className = "ball fa fa-spin fa-fw";
+}
 
-console.log(myVideo.currentSrc);
+function pauseVideo() {
+  var time = myVideo.currentTime.toFixed(2)
+  var pauseLog = 'Time: ' + time + '   Video Paused' + '<br>'
+  myVideo.pause();
+  activityLogArray.unshift(pauseLog);
+  activityLog.innerHTML+= pauseLog;
+  document.getElementById("ballTurtleRabbit").className = "ball fa fa-fw";
+}
+
+function advanceVideo() {
+  var time = myVideo.currentTime.toFixed(2)
+  var newTime = (myVideo.currentTime += 10.00).toFixed(2)
+  var advanceLog = 'Time: ' + time + '   Video Advanced to ' + (newTime) + '<br>'
+  activityLogArray.unshift(advanceLog);
+  activityLog.innerHTML+= advanceLog;
+}
+
+function rewindVideo() {
+  var time = myVideo.currentTime.toFixed(2)
+  var newTime = (myVideo.currentTime -= 10.00).toFixed(2)
+  var rewindLog = 'Time: ' + time + '   Video Rewind to ' + newTime + '<br>'
+  activityLogArray.unshift(rewindLog);
+  activityLog.innerHTML+= rewindLog;
+}
+
+function fasterVideo() {
+  var time = myVideo.currentTime.toFixed(2)
+  var rate = myVideo.playbackRate.toFixed(1)
+  var newRate = (myVideo.playbackRate += 0.1).toFixed(1)
+  var fasterLog = 'Time: ' + time + '   increase rate from ' + rate + ' to ' + (newRate) + '<br>'
+  activityLogArray.unshift(fasterLog);
+  activityLog.innerHTML+= fasterLog;
+  if (newRate > 1.0) {
+    document.getElementById('ballTurtleRabbit').src ="images/rabbit.png"
+  } else if (newRate < 1.0) {
+    document.getElementById('ballTurtleRabbit').src ="images/turtle.png"
+  } else {
+    document.getElementById('ballTurtleRabbit').src ="images/ball.png"
+  }
+}
+
+function slowerVideo() {
+  var time = myVideo.currentTime.toFixed(2)
+  var rate = myVideo.playbackRate.toFixed(1)
+  var newRate = (myVideo.playbackRate -= 0.1).toFixed(1)
+  var slowerLog = 'Time: ' + time + '   increase rate from ' + rate + ' to ' + (newRate) + '<br>'
+  activityLogArray.unshift(slowerLog);
+  activityLog.innerHTML+= slowerLog;
+  if (newRate > 1.0) {
+    document.getElementById('ballTurtleRabbit').src ="images/rabbit.png"
+  } else if (newRate < 1.0) {
+    document.getElementById('ballTurtleRabbit').src ="images/turtle.png"
+  } else {
+    document.getElementById('ballTurtleRabbit').src ="images/ball.png"
+  }
+}
+
+function louderVideo() {
+  var time = myVideo.currentTime.toFixed(2)
+  var volume = myVideo.volume.toFixed(1)
+  if (volume === 1 || volume === 1.0) {
+    alert('Maximum Volume')
+  }
+  var newVolume = (myVideo.volume += 0.1).toFixed(1)
+  var louderLog = 'Time: ' + time + '   increase volume from ' + volume + ' to ' + (newVolume) + '<br>'
+  activityLogArray.unshift(louderLog);
+  activityLog.innerHTML+= louderLog;
+}
+
+function quieterVideo() {
+  var time = myVideo.currentTime.toFixed(2)
+  var volume = myVideo.volume.toFixed(1)
+  if (volume === 0) {
+    alert('Minumum Volume')
+  }
+  var newVolume = (myVideo.volume -= 0.1).toFixed(1)
+  var quieterLog = 'Time: ' + time + '   increase volume from ' + volume + ' to ' + (newVolume) + '<br>'
+  activityLogArray.unshift(quieterLog);
+  activityLog.innerHTML+= quieterLog;
+}
 
 function loadRollerBabies () {
   source.setAttribute("src", 'videos/rollerbabiesEvian.mp4')
-  console.log(myVideo);
   myVideo.load()
-  return
+  var loadRollerBabiesLog = 'Enjoy watching Roller Babies' + '<br>'
+  activityLogArray.unshift(loadRollerBabiesLog);
+  activityLog.innerHTML+= loadRollerBabiesLog;
 }
 
 function loadBabyMe () {
   source.setAttribute("src", 'videos/babymeEvian.mp4')
-  console.log(myVideo);
   myVideo.load()
-  return
+  var loadBabyMeLog = 'Enjoy watching Baby Me' + '<br>'
+  activityLogArray.unshift(loadBabyMeLog);
+  activityLog.innerHTML+= loadBabyMeLog;
 }
 
 function loadSurfingBaby () {
   source.setAttribute("src", 'videos/babybayEvian.mp4')
-  console.log(myVideo);
   myVideo.load()
-  return
+  var loadSurfingBabyLog = 'Enjoy watching Surfing Babies' + '<br>'
+  activityLogArray.unshift(loadSurfingBabyLog);
+  activityLog.innerHTML+= loadSurfingBabyLog;
 }
